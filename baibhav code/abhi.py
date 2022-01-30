a4215aa88feb0690014be4cee9ef3955e790f408
abhiandjha <98700068+abhiandjha@users.noreply.github.com>
2022-01-30 21:17:12 +0530

    Add files via upload

---
+++ baibhav code/abhi.py
@@ -0,0 +1,9 @@
+largest = None 
+smallest = None
+while True:
+    num = input ("Enter a number")
+    if num == "done" :
+        break
+    try :
+        num = float(inp)
+        except 
---
+++ baibhav code/baibhav3.py
@@ -0,0 +1,5 @@
+hrs = input("Enter Hour: ")
+Rate = input("Enter Rate per Hour: ")
+pay = float(hrs)*float(Rate)
+
+print("Pay: ",pay)
